569df8219eeba4ded8c528dfa1f36988dcb7d9ba
Vincenzo <ilcontevince@gmail.com>
2020-08-07 20:37:38 +0100

    remove config folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config({ path: './config/config.env' });
+require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 const colors = require('colors');
