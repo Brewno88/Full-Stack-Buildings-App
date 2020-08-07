faa7d569a33275dfb6698b2a10914d44f8439003
Vincenzo <ilcontevince@gmail.com>
2020-08-07 13:15:27 +0100

    fixed setups before heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
@@ -9,17 +8,17 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-// Connect to DB
-mongoose.connect(process.env.DB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-});
+// Connect to MongoDB
+mongoose
+	.connect(process.env.DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log('Connected to MongoDB'))
+	.catch(err => console.log(err));
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log('Connected to MongoDB');
-});
+const PORT = process.env.PORT || 5000;
 
 // Routes
 const buildingsRouter = require('./routes/buildings');
@@ -28,14 +27,14 @@ app.use('/api/buildings', buildingsRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set a static folder
-	app.use(express.static('../build'));
+	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.get('/', (req, res) => {
 	res.send('Homepage');
 });
