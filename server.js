9ba8d6a7235bdf36d92e284bafc442adc3502d7c
Vincenzo <ilcontevince@gmail.com>
2020-08-07 13:45:49 +0100

    changed static folder path

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use('/api/buildings', buildingsRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set a static folder
-	app.use(express.static('client/build'));
+	app.use(express.static('./client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
 	});
 }
 
