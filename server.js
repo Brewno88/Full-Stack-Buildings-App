6f7e932e341b24a90819ba3c8b8b4710a72e12a4
Vincenzo <ilcontevince@gmail.com>
2020-08-07 18:25:57 +0100

    separate DB connection and .env file

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: './config/config.env' });
 const cors = require('cors');
 const path = require('path');
+const colors = require('colors');
 
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose
-	.connect(process.env.DB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	})
-	.then(() => console.log('Connected to MongoDB'))
-	.catch(err => console.log(err));
+const connectDB = require('./config/db');
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
@@ -34,7 +29,11 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
+);
 app.get('/', (req, res) => {
-	res.send('Homepage');
+	res.send('API Homepage');
 });
