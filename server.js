ebd77ca5badd8e644972ea145b53ce186be776eb
Vincenzo <ilcontevince@gmail.com>
2020-08-07 21:11:25 +0100

    fixed static path to "client/build"

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use('/api/buildings', buildingsRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set a static folder
-	app.use(express.static('./client/build'));
+	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
-	});
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
 }
 
 app.listen(PORT, () =>
